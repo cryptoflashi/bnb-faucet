3a61e874fd405f79c452f000b24d58d1413f2b45
Crypto Flash <cryptoflash@MacBook-Air-de-Crypto.local>
2025-04-29 20:24:38 -0500

    fix: removed local network config for Railway compatibility

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  defaultNetwork: "bnblocal",
-  networks: {
-    bnblocal: {
-      url: "http://127.0.0.1:8545",
-      chainId: 31337, // Similar al testnet de BSC
-      accounts: [
-        "0x619f8673a25be4109a0338808f69f398701f6bfead134a0828713262dd08bcb8"
-      ]
-    },
-  },
   solidity: "0.8.20",
 };
