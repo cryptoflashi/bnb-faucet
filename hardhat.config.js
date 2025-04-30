5035fb0e7722f3e1c7a94087d97b268e9864c602
Crypto Flash <cryptoflash@MacBook-Air-de-Crypto.local>
2025-04-29 22:30:10 -0500

    feat: configure BSC chain ID and local network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
+  defaultNetwork: "bnblocal",
+  networks: {
+    bnblocal: {
+      url: "http://0.0.0.0:8545",
+      chainId: 56, // BSC mainnet Chain ID
+      accounts: [
+        "0x619f8673a25be4109a0338808f69f398701f6bfead134a0828713262dd08bcb8"
+      ]
+    },
+  },
   solidity: "0.8.20",
 };
