253611842f5efa14ae90893ff1157fe3371518bb
cryptoflashi <136560614+cryptoflashi@users.noreply.github.com>
2025-10-10 23:32:10 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     bnblocal: {
       url: "https://bnb-faucet.onrender.com",
-      chainId: 56, // BSC mainnet Chain ID
+      chainId: 01, // BSC mainnet Chain ID
       accounts: [
         "0x619f8673a25be4109a0338808f69f398701f6bfead134a0828713262dd08bcb8"
       ]
